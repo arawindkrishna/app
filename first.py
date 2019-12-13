d81fcf57bbe2b5dbcdbe1b3c716be0f4e796a72a
Cloud User <ec2-user@ip-172-31-17-231.us-east-2.compute.internal>
2019-12-13 11:35:54 +0000

    jamaal app

---
+++ first.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+project_root=os.path.dirname(os.path.realpath(__file__))
+dbfile=os.path.join(project_root,'test.db')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+dbfile
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(80), unique=True, nullable=False)
+
+	def __repr__(self):
+		return "{}".format(self.text)
+
+	def __str__(self):
+		return unicode(self).encode('utf-8')
+
+	def __unicode__(self):
+		return self.text
+
+
+
+@app.route('/')
+def hello_world():
+	return render_template('view.html')
+
+@app.route('/write')
+def write_db():
+	textin=request.args.get('text1')
+	user=User(text=textin)
+	print(textin)
+	db.session.add(user)
+	db.session.commit()
+	return render_template('view.html')
+
+@app.route('/get')
+def get_db():
+	users = User.query.all()
+	# print(all_text)
+	return render_template('out.html', all_users=users)
+
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=8080)
